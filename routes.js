77636a97baad045d4a906ea540424d8ea0283b47
student <student@pop-os.localdomain>
2020-10-30 12:23:28 +1300

    functions: getAllPosts, getPostById and getAllTags are all working

--- routes.js
+++ routes.js
@@ -1,11 +1,46 @@
 const express = require('express')
 const { routes } = require('./server')
 const router = express.Router()
-const db = require('./db')//possible conflict in name different
+const db = require('./db')
+
 
 router.get('/' ,(req,res) => {
-    res.render('index')
+    const viewData = {}
+    db.getAllPosts()
+        .then(posts => {
+            viewData.posts = posts
+
+            db.getAllTags()
+            .then(tags => {
+                viewData.tags = tags
+                
+                console.log(viewData)
+                res.render('index', viewData)
+            })
+        })
+
 })
+
+router.get('/viewPost/:id', (req, res) => {
+    const urlId = req.params.id
+
+    db.getPost(urlId)
+    .then (postData => {
+
+        const viewData = {
+            id: postData.id,
+            title: postData.title,
+            content: postData.content,
+            url: postData.url,
+            author: postData.author
+        }
+
+        console.log(viewData)
+
+        res.render('postView', viewData)
+    })
+})
+
 
 router.get('/editPost/:id' ,(req,res) => {
     const urlId = req.params.id
@@ -28,23 +63,24 @@ router.get('/newPost', (req,res) => {
     res.render('newPost')
 })
 
-router.get('/viewPost/:id', (req, res) = >{ 
-    const urlId = req.params.id
 
-    db.getPost(urlId)
-    .then (postData => {
 
-        const viewData = {
-            id: postData.id,
-            title: postData.title,
-            content: postData.content,
-            url: postData.url,
-            author: postData.author
-        }
 
-        res.render('postView', viewData)
-    })
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 router.post('/newPost', (req, res) => {
 
--- seeds/posts.js
+++ seeds/posts.js
@@ -8,7 +8,7 @@ exports.seed = (knex) =>
           title: "cool video 1",
           content: "this is a cool video i found 1",
           url:
-            "https://www.youtube.com/watch?v=G7RgN9ijwE4&list=PLq2OOmsvIjzfxiJlyf4SwxQjAiystwbGh&index=1",
+            "https://www.youtube.com/embed/SZQhgExjBvQ",
           author: "mr 1",
         },
         {
@@ -16,7 +16,7 @@ exports.seed = (knex) =>
           title: "cool video 2",
           content: "this is a cool video i found 2",
           url:
-            "https://www.youtube.com/watch?v=D-UmfqFjpl0&list=PLq2OOmsvIjzfxiJlyf4SwxQjAiystwbGh&index=2",
+            "https://www.youtube.com/embed/SZQhgExjBvQ",
           author: "mr 2",
         },
         {
@@ -24,7 +24,7 @@ exports.seed = (knex) =>
           title: "cool video 3",
           content: "this is a cool video i found 3",
           url:
-            "https://www.youtube.com/watch?v=KmtzQCSh6xk&list=PLq2OOmsvIjzfxiJlyf4SwxQjAiystwbGh&index=3",
+            "https://www.youtube.com/embed/SZQhgExjBvQ",
           author: "mr 3",
         },
         {
@@ -32,7 +32,7 @@ exports.seed = (knex) =>
           title: "cool video 4",
           content: "this is a cool video i found 4",
           url:
-            "https://www.youtube.com/watch?v=ETbOX50t0kw&list=PLq2OOmsvIjzfxiJlyf4SwxQjAiystwbGh&index=4",
+            "https://www.youtube.com/embed/SZQhgExjBvQ",
           author: "mr 4",
         },
         {
@@ -40,7 +40,7 @@ exports.seed = (knex) =>
           title: "cool video 5",
           content: "this is a cool video i found 5",
           url:
-            "https://www.youtube.com/watch?v=pOeig6_aAtE&list=PLq2OOmsvIjzfxiJlyf4SwxQjAiystwbGh&index=5",
+            "https://www.youtube.com/embed/SZQhgExjBvQ",
           author: "mr 5",
         },
         {
@@ -48,7 +48,7 @@ exports.seed = (knex) =>
           title: "cool video 6",
           content: "this is a cool video i found 6",
           url:
-            "https://www.youtube.com/watch?v=zBJU9ndpH1Q&list=PLq2OOmsvIjzfxiJlyf4SwxQjAiystwbGh&index=6",
+            "https://www.youtube.com/embed/SZQhgExjBvQ",
           author: "mr 6",
         },
         {
@@ -56,7 +56,7 @@ exports.seed = (knex) =>
           title: "cool video 7",
           content: "this is a cool video i found 7",
           url:
-            "https://www.youtube.com/watch?v=y-P0m0M_8pc&list=PLq2OOmsvIjzfxiJlyf4SwxQjAiystwbGh&index=7",
+            "https://www.youtube.com/embed/SZQhgExjBvQ",
           author: "mr 7",
         },
         {
@@ -64,7 +64,7 @@ exports.seed = (knex) =>
           title: "cool video 8",
           content: "this is a cool video i found 8",
           url:
-            "https://www.youtube.com/watch?v=y-P0m0M_8pc&list=PLq2OOmsvIjzfxiJlyf4SwxQjAiystwbGh&index=8",
+            "https://www.youtube.com/embed/SZQhgExjBvQ",
           author: "mr 8",
         },
         {
@@ -72,7 +72,7 @@ exports.seed = (knex) =>
           title: "cool video 9",
           content: "this is a cool video i found 9",
           url:
-            "https://www.youtube.com/watch?v=y-P0m0M_8pc&list=PLq2OOmsvIjzfxiJlyf4SwxQjAiystwbGh&index=9",
+            "https://www.youtube.com/embed/SZQhgExjBvQ",
           author: "mr 9",
         },
         {
@@ -80,7 +80,7 @@ exports.seed = (knex) =>
           title: "cool video 10",
           content: "this is a cool video i found 10",
           url:
-            "https://www.youtube.com/watch?v=y-P0m0M_8pc&list=PLq2OOmsvIjzfxiJlyf4SwxQjAiystwbGh&index=10",
+            "https://www.youtube.com/embed/SZQhgExjBvQ0",
           author: "mr 10",
         },
       ])
